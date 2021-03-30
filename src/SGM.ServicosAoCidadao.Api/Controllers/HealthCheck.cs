b803e8975fa547112b47a9a33d4aad56f71a818e
Paresque <thyago.paresque@webiss.com.br>
2021-03-30 15:21:47 -0300

    Criando um healthcheck para o load balancer

---
+++ src/SGM.ServicosAoCidadao.Api/Controllers/HealthCheck.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGM.ServicosAoCidadao.Api.Controllers
+{
+	[ApiController]
+	[Route("/api/servicos-ao-cidadao/health-check")]
+	public class HealthCheck : Controller
+	{
+		public IActionResult Index()
+		{
+			return Ok();
+		}
+	}
+}
