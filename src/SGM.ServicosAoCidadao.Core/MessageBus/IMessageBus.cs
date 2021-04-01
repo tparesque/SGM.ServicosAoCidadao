561400abf38ebbcd565cd832aa1d9b4261c4f0cc
Paresque <thyago.paresque@webiss.com.br>
2021-03-31 21:49:09 -0300

    Extraindo interface para um arquivo próprio

---
+++ src/SGM.ServicosAoCidadao.Core/MessageBus/IMessageBus.cs
@@ -0,0 +1,18 @@
+﻿using SGM.ServicosAoCidadao.Core.Mensagens;
+using System;
+using System.Threading.Tasks;
+
+namespace SGM.ServicosAoCidadao.Core.MessageBus
+{
+	public interface IMessageBus : IDisposable
+	{
+		#region Padrão Publish/Subscribe
+
+		Task PublishAsync<T>(T message) where T : IntegrationEvent;
+
+		void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
+
+
+		#endregion
+	}
+}
--- src/SGM.ServicosAoCidadao.Core/MessageBus/MessageBus.cs
+++ src/SGM.ServicosAoCidadao.Core/MessageBus/MessageBus.cs
@@ -95,30 +95,4 @@ namespace SGM.ServicosAoCidadao.Core.MessageBus
 		#endregion
 
 	}
-
-	public interface IMessageBus : IDisposable
-	{
-
-		#region Padrão Publish/Subscribe
-
-		Task PublishAsync<T>(T message) where T : IntegrationEvent;
-
-		void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
-
-
-		#endregion
-
-		#region Padrão Request/Response
-
-		Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
-			where TRequest : IntegrationEvent
-			where TResponse : ResponseMessage;
-
-		Task<IDisposable> RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
-			where TRequest : IntegrationEvent
-			where TResponse : ResponseMessage;
-
-
-		#endregion
-	}
 }
